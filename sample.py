79f81f67caea7439e255dc7b2a8fafbfbf23972c
vignesh2k9 <vigneshselva2009@gmail.com>
2024-05-24 10:34:52 +0530

    initial commit

---
+++ sample.py
@@ -0,0 +1,5 @@
+print('divide two number')
+Num1 = int(input('enter the first number as numerator'))
+Num2 =  int(input('enter the second number as denominator'))
+Divide = Num1/Num2
+print('divide of two numbers:',Divide)
